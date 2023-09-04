0a1b099d19db676c279fc0ce17ecaadfc43621bb
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-04 13:35:38 +0200

    implement logout effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import {
 } from "@angular/router";
 import { AuthState } from "./auth/reducers";
 import { isLoggedIn, isLoggedOut } from "./auth/auth.selectors";
-import { logout } from "./auth/auth.actions";
+import { login, logout } from "./auth/auth.actions";
 
 @Component({
   selector: "app-root",
@@ -26,6 +26,12 @@ export class AppComponent implements OnInit {
   constructor(private router: Router, private store: Store<AuthState>) {}
 
   ngOnInit() {
+    const userProfile = localStorage.getItem("user");
+
+    if (userProfile) {
+      this.store.dispatch(login({ user: JSON.parse(userProfile) }));
+    }
+
     this.router.events.subscribe((event) => {
       switch (true) {
         case event instanceof NavigationStart: {
--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { AuthActions } from "./action-types";
 import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthEffects {
@@ -16,5 +17,17 @@ export class AuthEffects {
     { dispatch: false } //nie pozwala na odpalanie kolejnych akcji
   );
 
-  constructor(private actions$: Actions) {}
+  logout$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.logout),
+        tap((_) => {
+          localStorage.removeItem("user");
+          this.router.navigateByUrl("/login");
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(private actions$: Actions, private router: Router) {}
 }
