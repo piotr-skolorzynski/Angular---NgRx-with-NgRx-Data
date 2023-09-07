ae3ab3aa4aa911fb4e94c1c26a5ebdc201b0f61d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-07 14:48:42 +0200

    lessons pagination using ngrx data

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Course } from "../model/course";
-import { Observable, of } from "rxjs";
+import { Observable } from "rxjs";
 import { Lesson } from "../model/lesson";
-import { concatMap, tap, map } from "rxjs/operators";
+import { concatMap, tap, map, withLatestFrom } from "rxjs/operators";
 
 import { CourseEntityService } from "../services/course-entity.service";
 import { LessonEntityService } from "../services/lesson-entity.service";
@@ -15,6 +15,8 @@ import { LessonEntityService } from "../services/lesson-entity.service";
 })
 export class CourseComponent implements OnInit {
   course$: Observable<Course>;
+
+  loading$: Observable<boolean>;
 
   lessons$: Observable<Lesson[]>;
 
@@ -37,8 +39,28 @@ export class CourseComponent implements OnInit {
       )
     );
 
-    this.lessons$ = of([]);
+    this.lessons$ = this.lessonsService.entities$.pipe(
+      withLatestFrom(this.course$),
+      tap(([_, course]) => {
+        if (this.nextPage === 0) {
+          this.loadLessonsPage(course);
+        }
+      }),
+      map(([lessons, course]) =>
+        lessons.filter((lesson) => lesson.courseId === course.id)
+      )
+    );
+
+    this.loading$ = this.lessonsService.loading$;
   }
 
-  loadLessonsPage(course: Course) {}
+  loadLessonsPage(course: Course) {
+    this.lessonsService.getWithQuery({
+      courseId: course.id.toString(),
+      pageNumber: this.nextPage.toString(),
+      pageSize: "3",
+    });
+
+    this.nextPage += 1;
+  }
 }
